1e3edebacb69713bf1eb41a36bd894488fca3fae
Mamun8854 <mamunabdullah8854@gmail.com>
2023-09-09 13:41:43 +0600

    style added

--- calculator.py
+++ calculator.py
@@ -3,5 +3,9 @@ from tkinter import *
 root = Tk()
 root.title('Smart Calculator')
 root.config(bg="green")
+root.geometry("550x450+100+100")
+
+entryField = Entry(root, font=('arial', 20, 'bold'), bg='black', fg='white')
+entryField.grid(row=0, column=0)
 
 root.mainloop()
