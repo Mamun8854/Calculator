1a9feabc96e13c7fb6cb1255f24df4fcbed58636
Mamun8854 <mamunabdullah8854@gmail.com>
2023-09-10 10:43:06 +0600

    added button style

--- calculator.py
+++ calculator.py
@@ -3,9 +3,10 @@ from tkinter import *
 root = Tk()
 root.title('Smart Calculator')
 root.config(bg="green")
-root.geometry("550x450+100+100")
+root.geometry("680x490+100+100")
 
-entryField = Entry(root, font=('arial', 20, 'bold'), bg='black', fg='white')
+entryField = Entry(root, font=('arial', 20, 'bold'), bg='black', fg='white', bd=10, relief=SUNKEN, width=30)
 entryField.grid(row=0, column=0)
-
+button = Button(root, height=2, width=5, bd=2, relief=SUNKEN, text='C', bg="green", fg='white')
+button.grid(row=1, column=0)
 root.mainloop()
