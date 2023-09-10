a9171f58d5a8a598cc7b58aac1ae75e72afac546
Mamun8854 <mamunabdullah8854@gmail.com>
2023-09-10 18:46:11 +0600

    all button added1

--- calculator.py
+++ calculator.py
@@ -5,6 +5,10 @@ root.title('Smart Calculator')
 root.config(bg="green")
 root.geometry("680x490+100+100")
 
+logoImage = PhotoImage(file='./logo.jpg')
+logoLabel = Label(root, image=logoImage)
+logoLabel.grid(row=0, column=0)
+
 entryField = Entry(root, font=('arial', 20, 'bold'), bg='black', fg='white', bd=10, relief=SUNKEN, width=30)
 entryField.grid(row=0, column=0, columnspan=8)
 
@@ -20,7 +24,7 @@ column_value = 0
 for i in button_text_list:
     button = Button(root, height=2, width=5, bd=2, relief=SUNKEN, text=i, bg="green", fg='white',
                 font=('arial', 18, 'bold'), activebackground='black', activeforeground='white')
-    button.grid(row=row_value, column=column_value)
+    button.grid(row=row_value, column=column_value, pady=1)
     column_value+=1
     if column_value > 7:
         row_value+=1
